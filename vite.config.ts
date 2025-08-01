f966ba10ab93874969290e5613994e641789d506
Filip Chudoba <80604468+filipchudoba@users.noreply.github.com>
2025-08-01 17:49:44 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/iceva-north-aurora/',
   server: {
     host: "::",
     port: 8080,
